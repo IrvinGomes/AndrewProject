da31f98d5a71663540e4f727d34efdaf292288d9
AndrewHGomes <andrewvianigomes@gmail.com>
2022-06-13 07:32:39 -0300

    Correção do telefone

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,7 +4,12 @@ function reverterDados(form) {
     console.log(reverter(form.segundo_nome.value));
     console.log(reverter(form.sobrenome.value));
     console.log(reverter(form.email.value));
-
+    console.log(reverter(form.telefone.value));
+    document.getElementById("rnome").innerHTML = reverter(form.nome.value);
+    document.getElementById("rsegundo_nome").innerHTML = reverter(form.segundo_nome.value);
+    document.getElementById("rsobrenome").innerHTML = reverter(form.sobrenome.value);
+    document.getElementById("remail").innerHTML = reverter(form.email.value);
+    document.getElementById("rtelefone").innerHTML = reverter(form.telefone.value);
 }
 function reverter(texto) {
     console.log(texto);
